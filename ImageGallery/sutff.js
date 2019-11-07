5df9adb3ca4793a1bbe2f65fc1b5b963b36362bb
Luricha <jason.chen53@myhunter.cuny.edu>
2019-11-07 14:56:09 -0500

    updated weblinks

---
+++ ImageGallery/sutff.js
@@ -0,0 +1,10 @@
+var username = prompt("Welcome to my Image Gallery. What is your name?")
+alert("Nice to meet you, " + username)
+var age = prompt("How old are you?")
+if (age < 21){
+	alert("Haha, you are not able to drink just yet!")
+}
+else{
+	alert("Great! It seems you are old enough to drink!")
+}
+console.log("Name is: " + username)
